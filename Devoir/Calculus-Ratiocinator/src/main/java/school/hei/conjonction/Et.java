1769685cf9ba8faa033da0cd570f2dad0e7165f1
KevinDonovan2 <hei.donovan.2@gmail.com>
2024-06-07 16:46:55 +0300

    feat: conjonction

---
+++ Devoir/Calculus-Ratiocinator/src/main/java/school/hei/conjonction/Et.java
@@ -0,0 +1,28 @@
+package school.hei.conjonction;
+import school.hei.Affirmation.Affirmation;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Et extends Affirmation {
+    private Affirmation affirmation_1, affirmation_2;
+
+    public Et(Affirmation affirmation_1, Affirmation affirmation_2) {
+        super(affirmation_1.getDescription() + "Et" + affirmation_2.getDescription());
+        this.affirmation_1 = affirmation_1;
+        this.affirmation_2 = affirmation_2;
+    }
+
+    @Override
+    public String evaluer() {
+        String premiere_affirmation = affirmation_1.evaluer();
+        String deuxieme_affirmation = affirmation_2.evaluer();
+
+        if(premiere_affirmation.equals("vrai") && deuxieme_affirmation.equals("vrai")){
+            return "vrai";
+        }else {
+            return "faux";
+        }
+    }
+}
